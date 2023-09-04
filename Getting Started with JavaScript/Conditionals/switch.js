1371c951b2bc507973af6f554e9e09228f21ae45
@nain333 <hnain70@gmail.com>
2023-09-04 18:22:44 +0530

    switch case and ternary

---
+++ Getting Started with JavaScript/Conditionals/switch.js
@@ -0,0 +1,17 @@
+{
+    const fruits = prompt("Enter the fruit")
+    switch(fruits){
+        case 'Apple':
+            console.log('Apple')
+        break;
+        case  'Kiwi':
+            console.log("kiwi")
+        default:
+            console.log("No it's neighter Apple nor Kiwi")
+            break;
+
+        
+
+    }
+    console.log('Outside Switch!')
+}
---
+++ Getting Started with JavaScript/Conditionals/ternary.js
@@ -0,0 +1,4 @@
+{
+  const number = prompt('Enter a number: ')
+  number % 2 === 0 ? alert('Even') : alert('Odd')
+}
