7c8560b7f3a137d93ee41de127fdd181096ab618
@nain333 <hnain70@gmail.com>
2023-09-06 17:54:46 +0530

    forLoop

--- Getting Started with JavaScript/Conditionals/ternary.js
+++ Getting Started with JavaScript/Conditionals/ternary.js
@@ -1,4 +1,11 @@
 {
   const number = prompt('Enter a number: ')
   number % 2 === 0 ? alert('Even') : alert('Odd')
+
+ let a = '4';
+ let b = '6';
+ const result = ++a + b--;
+ console.log(typeof(result))
+
+
 }
---
+++ loops, arrays functions and object/Loops/forLoop/forLoop.js
@@ -0,0 +1,8 @@
+{
+    // print odd nubmers from 0 to 50
+for(let num = 0 ; num <= 50; num+=2){
+    console.log(num)
+}
+
+
+}
