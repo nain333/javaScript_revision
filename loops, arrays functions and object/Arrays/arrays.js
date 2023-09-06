b422bf886c04055f045387ce0115b2ee642d2caa
@nain333 <hnain70@gmail.com>
2023-09-06 23:08:04 +0530

    Arrays

---
+++ loops, arrays functions and object/Arrays/arrays.js
@@ -0,0 +1,13 @@
+/*Arrays*/
+/*Array is a dataStructure which stores dataItems in a contigious memory*/ 
+const subjects=['Data Structures','Compiler Design','Algorithm and Design']
+console.log(subjects)
+// second way to intialize an array:
+const cars= new Array('BMW','Audi','Rolls Royce')
+console.log(cars)
+const arr = ['Apple','nain',4,true]
+console.log(arr)
+// in javaScript array is hetrogenous
+console.log(arr(3))
+// accessing the element of array indexing starts with 0
+ 
