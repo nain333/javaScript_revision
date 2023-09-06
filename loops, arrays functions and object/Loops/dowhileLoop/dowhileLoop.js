8f704def07ecfbcc660df7b23e4db872ec5a0e72
@nain333 <hnain70@gmail.com>
2023-09-06 19:15:38 +0530

    do While Loop

---
+++ loops, arrays functions and object/Loops/dowhileLoop/dowhileLoop.js
@@ -0,0 +1,16 @@
+/* Do while Loop 
+    do{//body}while(condition)
+        it's an exit control Loop
+        itrates atelast one time
+
+*/ 
+console.log('doWhile')
+let num;
+let count=0;
+do{
+    num = prompt('Enter the number: ')
+    count+=1
+
+
+}while(num<=50)
+console.log(count)
