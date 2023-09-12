18062547eb0fce9b27a009b82db4cfdb5e2d7718
@nain333 <hnain70@gmail.com>
2023-09-12 14:23:56 +0530

    calculatePrice, URL constructor

--- loops, arrays functions and object/Objects/objects.js
+++ loops, arrays functions and object/Objects/objects.js
@@ -57,4 +57,53 @@ console.log(student[333])
     }
     
     console.log(findClassTopper(studentMarks)); // Output: "Jane"
+
+    //complete the calculatePrice function
+
+    function calculatePrice(goods) {
+        // Implement your function here
+        let totalPrice = 0;
+        
+        for (let product in goods) {
+            // Access the value associated with the property using goods[product]
+            totalPrice += goods[product].price * goods[product].quantity;
+        }
+        
+        return totalPrice;
+    }
     
+    const goods = {
+        product1: { price: 100, quantity: 3 },
+        product2: { price: 50, quantity: 2 },
+        product3: { price: 200, quantity: 1 }
+    };
+    
+    console.log(calculatePrice(goods)); // Output: 650
+    
+    //Complete the URLconstructor function 
+// Do not change the starter code. 
+
+function URLconstructor(queryParameters, domain, path) {
+    // Initialize an empty array to store query parameter strings
+    const queryParams = [];
+
+    // Loop through the queryParameters object and build the query parameter strings
+    for (const key in queryParameters) {
+        if (queryParameters.hasOwnProperty(key)) {
+            queryParams.push(`${key}=${queryParameters[key]}`);
+        }
+    }
+
+    // Combine the domain, path, and query parameters into the URL string
+    const queryString = queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
+    const url = `https://${domain}${path}${queryString}`;
+
+    return url;
+}
+
+const queryParameters = { name: 'John', age: '28' };
+const domain = "google.com";
+const path = '/search';
+
+console.log(URLconstructor(queryParameters, domain, path));
+// Output: https://google.com/search?name=John&age=28
